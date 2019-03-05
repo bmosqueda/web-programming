6ce028f3d01501e9c02fe966f6b45de2009ba215
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-05 08:22:46 -0600

    Third pattern, return a new instance of a class (or function constructor)

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ greet();
 
 var greet2 = require('./greet2').greet;
 greet2();
+
+var greet3 = require('./greet3');
+console.log(greet3);
+// console.log(greet3.greeting);
+console.log(greet3.greet());
---
+++ greet3.js
@@ -0,0 +1,11 @@
+/*
+  Third pattern, return a new instance of a class (or function constructor)
+ */
+function Greeter() {
+  this.greeting = 'Hello world from Greeter class';
+
+  this.greet = function greet() {
+    console.log(this.greeting);
+  }; 
+}
+module.exports = new Greeter();
