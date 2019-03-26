5ab4ff96b12e050bfdd197454bd2751282a2e073
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-26 08:35:49 -0600

    Push example whitouht set upstream

--- app.js
+++ app.js
@@ -5,9 +5,3 @@ const person = new Person('Brandon', 'Mosqueda', 1698);
 const police = new Policeman('Alejandro', 'González');
 
 police.greet();
-
-console.log(person);
-console.log(person.__proto__.__proto__);
-console.log('**************');
-console.log(police);
-console.log(police.__proto__.__proto__);
