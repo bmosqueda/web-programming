47f5ee94acedde1cb9e8cd02c9b184d99616bc6e
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-03-26 08:34:51 -0600

    Pruebas clase Persona y policía

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const Person = require('./models/person-class');
 const Policeman = require('./models/policeman-proto');
 
-const police = new Policeman('Brandon', 'Mosqueda', 1698);
+const person = new Person('Brandon', 'Mosqueda', 1698);
+const police = new Policeman('Alejandro', 'González');
 
 police.greet();
+
+console.log(person);
+console.log(person.__proto__.__proto__);
+console.log('**************');
+console.log(police);
+console.log(police.__proto__.__proto__);
--- models/policeman-proto.js
+++ models/policeman-proto.js
@@ -4,6 +4,7 @@ var util = require('util');
 var Person = require('./person-proto');
 
 function Policeman(firstname, lastname, badgenumber = 1234) {
+  //Para inicializar las variables de la clase que está heredando
   Person.call(this, firstname, lastname);
   this.badgenumber = badgenumber;
 }
