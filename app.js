e8a39e0c310d4cadeccc2932ea3bf6a4d7f36aa3
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-04-04 08:04:26 -0600

    Template example whit hmtl file

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-type': 'text/html'});
-  const html = fs.readFileSync(__dirname + '/index.htm');
+  var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+  var message = 'Hello Brandon';
+  html = html.replace('{Message}', message);
   res.end(html);
 
 }).listen(1337, '127.0.0.1');
