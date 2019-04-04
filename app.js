3d0bd250921dfd15942cf1991f6e76fbd28a5e0a
Brandon Mosqueda <bmosqueda@ucol.mx>
2019-04-04 08:30:21 -0600

    Management of different routes whitin our server

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-type': 'application/json'});
-  var obj = {
-    firstname: 'Brandon',
-    lastname: 'Mosqueda'
-  };
+  if(req.url === '/') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
 
-  res.end(JSON.stringify(obj));
+  } else if(req.url === '/api') {
+    res.writeHead(200, {'Content-type': 'application/json'});
+    var obj = {
+      firstname: 'Brandon',
+      lastname: 'Mosqueda'
+    };
+
+    res.end(JSON.stringify(obj));
+
+  } else {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end('<h1>404 NOT FOUND</h1>');
+
+  }
 
 }).listen(1337, '127.0.0.1');
